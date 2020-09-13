4b7bcb237f3c6c4699e665a7577dff71d0f7d082
radlee <prof.lolito@gmail.com>
2020-09-14 01:56:33 +0200

    Fb

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -20,6 +20,12 @@ export default class Footer extends Component {
 
                                 <div className="twelve columns">        
                                     <div class="widget">
+                                        <hr className="my-2" />
+
+                                        <div class="fb-like" data-href="https://radlee.github.io/origin2020/" data-width="" data-layout="button_count" data-action="like" data-size="large" data-share="true"></div>
+                                        
+                                        <hr className="my-2" /> 
+                                        
                                         <h1 className="footer-text">OR<span className="ig">IG</span>IN<span className="ig">2020</span></h1>
                                     </div>
                                 </div>
